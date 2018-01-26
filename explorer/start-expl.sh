5611f867491e3dfd59212bfd958b477d9148c09d
Bill Lv <bill@nebulas.io>
2018-01-26 16:12:37 +0800

    Changed dev not to initialize data

--- explorer/start-expl.sh
+++ explorer/start-expl.sh
@@ -1,1 +1,1 @@
-java -Xms256m -Xmx256m -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8085,suspend=n -jar ./build/libs/explorer-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.initialize=true  >console.log 2>&1 &
+java -Xms256m -Xmx256m -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8085,suspend=n -jar ./build/libs/explorer-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.initialize=false  >console.log 2>&1 &
