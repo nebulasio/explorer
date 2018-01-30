e381859e5facd3e87ebad36892d110c42b9446ae
Bill Lv <bill@nebulas.io>
2018-01-30 13:16:11 +0800

    Added app 404

--- explorer/src/main/java/io/nebulas/explorer/config/GlobalExceptionHandler.java
+++ explorer/src/main/java/io/nebulas/explorer/config/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package io.nebulas.explorer.config;
 
 import com.google.common.base.Throwables;
 import io.nebulas.explorer.exception.NotFoundException;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,8 +22,11 @@ import javax.servlet.http.HttpServletRequest;
  * @since 2018-01-30
  */
 @Slf4j
+@AllArgsConstructor
 @ControllerAdvice(basePackages = "io.nebulas.explorer.controller")
 public class GlobalExceptionHandler {
+    private final YAMLConfig config;
+
     @ExceptionHandler(NotFoundException.class)
     public ModelAndView handleError(HttpServletRequest req, Exception exception)
             throws Exception {
@@ -33,11 +37,15 @@ public class GlobalExceptionHandler {
 
         log.error("Request: " + req.getRequestURI() + " raised " + exception);
 
+        PageConfig pageConfig = config.getPage();
+
         ModelAndView mav = new ModelAndView();
         mav.addObject("status", 404);
         mav.addObject("error", "Not Found");
         mav.addObject("message", Throwables.getRootCause(exception).getMessage());
-        mav.setViewName("error");
+        mav.addObject("baseUrl", pageConfig.getBaseUrl());
+        mav.addObject("hostUrl", pageConfig.getHostUrl());
+        mav.setViewName("error/404");
         return mav;
     }
 }
