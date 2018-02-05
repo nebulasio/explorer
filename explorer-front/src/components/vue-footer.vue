ceb3d7e828e7ee1be4949636c7cafab9f437af5c
zhechen <15010159959@163.com>
2018-02-05 18:13:08 +0800

    footer

--- explorer-front/src/components/vue-footer.vue
+++ explorer-front/src/components/vue-footer.vue
@@ -56,36 +56,31 @@
     .vue-footer .footer_link-list_banner {
         height: 30px;
     }
+    .vue-footer .footer_link-list_banner a{
+        color: #fff;
+    }
 </style>
 <template>
     <div class="vue-footer">
         <div class="container">
             <div class="row">
-                <div class="footer_left col-md-3 ">
-                    <div class="footer_left_up ">
-                        <img src="/static/img/logo.png ">
-                    </div>
-                    <div class="footer_left_down ">
-                        <p>Nebulas is a Block Explorer and Analytics Platform for Ethereum, a decentralized smart contracts platform.</p>
-                    </div>
-                </div>
 
-                <div class="footer_right col-md-9 ">
+                <div class="footer_right col-md-12 ">
                     <div class="footer_banner_tab ">
                         <h4>Links</h4>
                     </div>
                     <div class="footer_link-list ">
                         <div class="footer_link-list_banner ">
-                            <a href="https://nebulas.io "> Nebulas.io </a>
+                            <a href="https://nebulas.io " target=view_blank > Nebulas.io </a>
                         </div>
                         <div class="footer_link-list_banner ">
-                            <a href="https://github.com/nebulasio">github</a>
+                            <a href="https://github.com/nebulasio" target=view_blank >github</a>
                         </div>
                         <div class="footer_link-list_banner ">
-                            <a href="https://testnet.nebulas.io/   ">testnet</a>
+                            <a href="https://testnet.nebulas.io" target=view_blank >testnet</a>
                         </div>
                         <div class="footer_link-list_banner ">
-                            <a href="# ">mainnet</a>
+                            <a href="#">mainnet</a>
                         </div>
                     </div>
                 </div>
