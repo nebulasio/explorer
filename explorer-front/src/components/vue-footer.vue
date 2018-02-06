27e83e42b559f324a1c7c1b38c16e44aa32b1963
zhechen <15010159959@163.com>
2018-02-06 12:06:02 +0800

    footer change

--- explorer-front/src/components/vue-footer.vue
+++ explorer-front/src/components/vue-footer.vue
@@ -8,24 +8,6 @@
         background-color: transparent;
     }
 
-    .vue-footer .footer_left {
-        height: 300px;
-    }
-
-    .vue-footer .footer_left_up {
-        height: 100px;
-        text-align: center;
-        margin-top: 6%;
-    }
-
-    .vue-footer .footer_left_down {
-        color: #fff;
-    }
-
-    .vue-footer .footer_middle {
-        height: 300px;
-    }
-
     .vue-footer .footer_banner_tab {
         border-bottom: 1px dashed #fff;
         color: #fff;
@@ -73,11 +55,15 @@
                         <div class="footer_link-list_banner ">
                             <a href="https://nebulas.io " target=view_blank > Nebulas.io </a>
                         </div>
-                        <div class="footer_link-list_banner ">
-                            <a href="https://github.com/nebulasio" target=view_blank >github</a>
-                        </div>
-                        <div class="footer_link-list_banner ">
-                            <a href="https://testnet.nebulas.io" target=view_blank >testnet</a>
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="footer_link-list_banner col-md-3 ">
+                                <a href="https://github.com/nebulasio" target=view_blank >github</a>
+                            </div>
+                            <div class="footer_link-list_banner col-md-3 ">
+                                <a href="https://testnet.nebulas.io" target=view_blank >testnet</a>
+                            </div>
+                            <div class="col-md-3"></div>
                         </div>
                         <div class="footer_link-list_banner ">
                             <a href="#">mainnet</a>
