fc8ea84c307db71642676d7b81ad5da9bd1f31d1
nathan wang <nathan@nebulas.io>
2018-02-06 17:12:12 +0800

    fix bug dynasties

--- explorer/src/main/java/io/nebulas/explorer/mapper/NebDynastyMapper.java
+++ explorer/src/main/java/io/nebulas/explorer/mapper/NebDynastyMapper.java
@@ -2,6 +2,7 @@ package io.nebulas.explorer.mapper;
 
 import io.nebulas.explorer.domain.NebDynasty;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ import java.util.List;
 @Mapper
 public interface NebDynastyMapper {
 
-    Integer batchAdd(List<NebDynasty> dynasties);
+    Integer batchAdd(@Param("dynasties") List<NebDynasty> dynasties);
 
     List<NebDynasty> findByBlockHeight(Long blockHeight);
 }
