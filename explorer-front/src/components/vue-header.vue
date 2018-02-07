a7e219a644c7421700d22d25744011070cb83f85
zhechen <15010159959@163.com>
2018-02-07 15:17:14 +0800

    logo img

--- explorer-front/src/components/vue-header.vue
+++ explorer-front/src/components/vue-header.vue
@@ -35,7 +35,7 @@
     <nav class="bg-light navbar navbar-expand-lg navbar-light vue-header">
         <div class=container>
             <router-link to=/ class=navbar-brand>
-                <img src="/static/img/logo.png" alt="">
+                <img src="/static/img/logo.png" width=150 alt="">
             </router-link>
             <button class=navbar-toggler type=button data-toggle=collapse data-target=#navbarSupportedContent aria-controls=navbarSupportedContent aria-expanded=false aria-label="Toggle navigation">
                 <span class=navbar-toggler-icon></span>
