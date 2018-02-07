355a8c366902e458e4624ad64fee0623b3374677
wangzimei <wzm@nebulas.io>
2018-02-07 16:38:06 +0800

    utility.js!toWei: make sure n is number

--- explorer-front/src/routes/accounts.vue
+++ explorer-front/src/routes/accounts.vue
@@ -55,43 +55,40 @@
                 currentPage: 0,
                 totalAccounts: 0,
                 totalBalance: 0,
-                totalPage: 1 // 为了允许 mounted 调用 nthPage
+                totalPage: 0
             };
         },
         methods: {
             nthPage(p) {
                 if (p)
-                    if (0 < p && p < this.totalPage + 1)
-                        if (p == this.currentPage)
-                            console.log("nthPage - 请求的第", p, "页正是当前页, 忽略此次调用");
-                        else if (this.ajaxing)
-                            console.log("nthPage - 上一个 ajax 还未返回, 忽略此次调用");
-                        else {
-                            this.ajaxing = true;
+                    if (p == this.currentPage)
+                        console.log("nthPage - 请求的第", p, "页正是当前页, 忽略此次调用");
+                    else if (this.ajaxing)
+                        console.log("nthPage - 上一个 ajax 还未返回, 忽略此次调用");
+                    else {
+                        this.ajaxing = true;
 
-                            api.getAccount(p, o => {
-                                this.ajaxing = false;
-                                this.arr = o.addressList;
-                                this.currentPage = o.page;
-                                this.totalAccounts = o.totalAccountsCnt;
-                                this.totalBalance = o.totalBalance;
-                                this.totalPage = o.totalPage;
+                        api.getAccount(p, o => {
+                            this.ajaxing = false;
+                            this.arr = o.addressList;
+                            this.currentPage = o.page;
+                            this.totalAccounts = o.totalAccountsCnt;
+                            this.totalBalance = o.totalBalance;
+                            this.totalPage = o.totalPage;
 
-                                if (this.arr.length) {
-                                    this.heightFrom = this.arr[0].height;
-                                    this.heightTo = this.arr[this.arr.length - 1].height;
-                                } else {
-                                    this.heightFrom = 0;
-                                    this.heightTo = 0;
-                                }
-                            }, xhr => {
-                                console.log(xhr);
-                                // this.ajaxing = false; // 由于跳转了所以不需要修改 ajaxing
-                                this.$router.replace("/404!" + this.$route.fullPath);
-                            });
-                        }
-                    else
-                        console.log("nthPage - 请求的第", p, "页不在 [ 1,", this.totalPage, "] 内, 忽略此次调用");
+                            if (this.arr.length) {
+                                this.heightFrom = this.arr[0].height;
+                                this.heightTo = this.arr[this.arr.length - 1].height;
+                            } else {
+                                this.heightFrom = 0;
+                                this.heightTo = 0;
+                            }
+                        }, xhr => {
+                            console.log(xhr);
+                            // this.ajaxing = false; // 由于跳转了所以不需要修改 ajaxing
+                            this.$router.replace("/404!" + this.$route.fullPath);
+                        });
+                    }
                 else
                     console.log("nthPage - 无效的 p", p, ", 忽略此次调用");
             },
@@ -110,7 +107,6 @@
         },
         mounted() {
             this.nthPage(1);
-            this.totalPage = 0;
         }
     };
 </script>
