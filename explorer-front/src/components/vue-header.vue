ada3da2330c8299de8b92c08b9792154bb72e2bf
zhechen <15010159959@163.com>
2018-02-07 14:12:36 +0800

    header logo

--- explorer-front/src/components/vue-header.vue
+++ explorer-front/src/components/vue-header.vue
@@ -34,7 +34,9 @@
 <template>
     <nav class="bg-light navbar navbar-expand-lg navbar-light vue-header">
         <div class=container>
-            <router-link to=/ class=navbar-brand></router-link>
+            <router-link to=/ class=navbar-brand>
+                <img src="/static/img/logo.png" alt="">
+            </router-link>
             <button class=navbar-toggler type=button data-toggle=collapse data-target=#navbarSupportedContent aria-controls=navbarSupportedContent aria-expanded=false aria-label="Toggle navigation">
                 <span class=navbar-toggler-icon></span>
             </button>
