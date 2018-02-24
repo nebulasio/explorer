64ec0ec4380a5a84f4d19cc557a8c5b049c140ab
nathan wang <nathan@nebulas.io>
2018-02-24 16:08:58 +0800

    update address balance

--- explorer-backend/src/main/java/io/nebulas/explorer/service/thirdpart/nebulas/bean/GetAccountStateRequest.java
+++ explorer-backend/src/main/java/io/nebulas/explorer/service/thirdpart/nebulas/bean/GetAccountStateRequest.java
@@ -9,11 +9,16 @@ import java.io.Serializable;
 /**
  * Desc:
  * User: nathan
- * Date: 2018-02-23
+ * Date: 2018-02-24
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetDynastyRequest implements Serializable {
-    private Long height;
+public class GetAccountStateRequest implements Serializable {
+    private String address;
+    private String block = "latest";
+
+    public GetAccountStateRequest(String address) {
+        this.address = address;
+    }
 }
--- explorer-backend/src/main/java/io/nebulas/explorer/service/thirdpart/nebulas/bean/GetBlockByHashRequest.java
+++ explorer-backend/src/main/java/io/nebulas/explorer/service/thirdpart/nebulas/bean/GetBlockByHashRequest.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
  * Desc:
  * User: nathan
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetBlockByHashRequest {
+public class GetBlockByHashRequest implements Serializable {
     private String hash;
     private boolean fullTransaction;
 }
