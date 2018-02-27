0c889f783a3d9f175f6f935d4fb5d82cb18e6765
wangzimei <wzm@nebulas.io>
2018-02-27 20:58:26 +0800

    frontend - adjust ui

--- explorer-front/src/routes/oops.vue
+++ explorer-front/src/routes/oops.vue
@@ -3,17 +3,18 @@
         height: 400px;
         background: url(/static/img/oops.jpg) center no-repeat;
     }
-    .vue-oops pre{
-        font-size: 44px;
-    }
 
+    .vue-oops .keyword {
+        font-size: 44px;
+        overflow-wrap: break-word;
+    }
 </style>
 <template>
     <div class="container vue-oops">
         <div class=text-center>
             <h1>Oops!</h1>
             <h3>The search string you entered was:</h3>
-            <pre>{{ $root.search }}</pre>
+            <div class=keyword>{{ $root.search }}</div>
             <h4>Sorry! This is an invalid search string.</h4>
         </div>
         <div class=image></div>
