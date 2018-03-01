c7b1dd6644154db7c951b4bbf1722344465c3303
wangzimei <wzm@nebulas.io>
2018-03-01 13:04:03 +0800

    vue-footer.vue - add icon

--- explorer-front/src/components/vue-footer.vue
+++ explorer-front/src/components/vue-footer.vue
@@ -18,6 +18,11 @@
     .vue-footer .container {
         margin-bottom: 30px;
     }
+
+    .vue-footer .fa {
+        text-align: center;
+        width: 14px;
+    }
 </style>
 <template>
     <div class=vue-footer>
@@ -27,28 +32,35 @@
                 <div class=col-1></div>
                 <div class=col-2>
                     <div>
+                        <!-- <span class="fa fa-(nebulas?)" aria-hidden=true></span> -->
                         <a target=_blank href=https://nebulas.io>Nebulas.io</a>
                     </div>
                     <div>
+                        <span class="fa fa-facebook-f" aria-hidden=true></span>
                         <a target=_blank href=https://www.facebook.com/nebulasproject>Facebook</a>
                     </div>
                     <div>
-                        <a target=_blank href=https://nebulasio.herokuapp.com>#Slack</a>
+                        <span class="fa fa-slack" aria-hidden=true></span>
+                        <a target=_blank href=https://nebulasio.herokuapp.com>Slack</a>
                     </div>
                     <div>
+                        <span class="fa fa-twitter" aria-hidden=true></span>
                         <a target=_blank href=https://twitter.com/nebulasio>Twitter</a>
                     </div>
                 </div>
                 <div class=col-1></div>
-                <div class=col-2>
+                <div class=col-3>
                     <div>
+                        <span class="fa fa-github" aria-hidden=true></span>
                         <a target=_blank href=https://github.com/nebulasio>Github.com/nebulasio</a>
                     </div>
                     <div>
+                        <span class="fa fa-github" aria-hidden=true></span>
                         <a target=_blank href=https://github.com/nebulasio/explorer/issues>Issue</a>
                     </div>
-                    <div>Commit:
-                        <a target=_blank href=https://github.com/nebulasio/explorer/commit/cf54099633db072aef15cec338a0ddaf21bb182a>cf54099</a>
+                    <div>
+                        <span class="fa fa-github" aria-hidden=true></span>
+                        <a target=_blank href=https://github.com/nebulasio/explorer/commit/cf54099633db072aef15cec338a0ddaf21bb182a>Commit: cf54099</a>
                     </div>
                 </div>
             </div>
