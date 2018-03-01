42ed1628e2ab17210c58bb07015970e206b25817
zhechen <15010159959@163.com>
2018-03-01 14:29:08 +0800

    pending 状态完善

--- explorer-front/src/routes/home.vue
+++ explorer-front/src/routes/home.vue
@@ -196,7 +196,7 @@
                         <div class=msg>
                             <div class=msg_change>
                                 <span class="msg_change_left">24h Change : </span>
-                                <span class="msg_change_right red">{{ trends=1 ? '+' : '-' }} {{ market.change24h }}%</span>
+                                <span class="msg_change_right" v-bind:class="[ market.trends == 1 ? 'red' : 'green']" >{{ market.trends == 1 ? '+' : '-' }} {{ market.change24h }}%</span>
                             </div>
                             <div class=msg_volume>
                                 <span class="msg_change_left">24h Volume :</span>
