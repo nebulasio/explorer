c10bf8cbb7a697e17babc7285dfe6c2441e6abb6
kurry <kurry@kurrydeMacBook-Air.local>
2018-07-09 20:57:15 +0800

    change value number

--- explorer-front/src/routes/address.vue
+++ explorer-front/src/routes/address.vue
@@ -149,7 +149,7 @@
                             <span v-if="o.to.hash == $route.params.id">{{ o.to.alias || o.to.hash }}</span>
                             <router-link v-else v-bind:to='fragApi + "/address/" + o.to.hash'>{{ o.to.alias || o.to.hash }}</router-link>
                         </td>
-                        <td>{{ easyNumber(o.value/1000000000000000000) }} NAS</td>
+                        <td>{{ numberAddComma( parseFloat(o.value/1000000000000000000).toPrecision(15) )  }} NAS</td>
                         <td class=txfee>
                             <span v-if=o.block.height>{{ toWei(o.txFee) }}</span>
                             <i v-else>(pending)</i>
