97ebe4ac3a59cbe97b4b767e0d0f8eb5befc7f9a
于宙Ryan <yuzhouwww@gmail.com>
2018-11-21 17:31:22 +0800

    nrc20 surpport

--- explorer-front/src/assets/app-config.js
+++ explorer-front/src/assets/app-config.js
@@ -7,13 +7,15 @@ module.exports = {
         // first item is default
         mainnet: {
             name: "Mainnet",
-            // url: "http://172.16.15.94:8080/api/" // 旭存的ip
-            url: "https://explorer.nebulas.io/main/api/",
+            // url: "http://172.16.15.94:8080/api/", // 旭存的ip
+            url: "http://47.52.137.118:8080/api/", // xihao的ip
+            // url: "https://explorer.nebulas.io/main/api/",
             atp: "n1zUNqeBPvsyrw5zxp9mKcDdLTjuaEL7s39"
         },
         testnet: {
             name: "Testnet",
-            url: "https://explorer.nebulas.io/test/api/",
+            url: "http://47.52.137.118:8080/api/", // xihao的ip
+            // url: "https://explorer.nebulas.io/test/api/",
             atp: "n1rR5uiy4vDUn7TPMAtJ8Y1Eo54K6EYvSJ6"
         }
     }
