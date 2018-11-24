06fa1a9172279a85b761cb0d7005d7c6e2a849a1
xihao.liao <xihao.liao@nebulas.io>
2018-11-24 17:13:48 +0800

    nas price set scale 2

--- explorer-backend/src/main/java/io/nebulas/explorer/service/blockchain/NebMarketCapitalizationService.java
+++ explorer-backend/src/main/java/io/nebulas/explorer/service/blockchain/NebMarketCapitalizationService.java
@@ -5,6 +5,8 @@ import io.nebulas.explorer.mapper.NebMarketCapitalizationMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 /**
  * nebulas market capitalization related operation service
  *
@@ -36,7 +38,10 @@ public class NebMarketCapitalizationService {
      * @return nebulas market capitalization information
      */
     public NebMarketCapitalization getLatest() {
-        return marketCapitalizationMapper.getLatest();
+
+        NebMarketCapitalization marketCapitalization = marketCapitalizationMapper.getLatest();
+        marketCapitalization.setPrice(marketCapitalization.getPrice().setScale(2, BigDecimal.ROUND_HALF_UP));
+        return marketCapitalization;
     }
 
     /**
