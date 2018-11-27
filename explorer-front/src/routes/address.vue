040ff4c5e48c9af051ccd0588b40fe5cb90bf656
于宙Ryan <yuzhouwww@gmail.com>
2018-11-27 19:42:34 +0800

    fix 合约代码

--- explorer-front/src/routes/address.vue
+++ explorer-front/src/routes/address.vue
@@ -375,7 +375,8 @@
         prism = require("prismjs"),
         jsBeautify = require("js-beautify").js_beautify,
         utility = require("@/assets/utility"),
-        BigNumber = require("bignumber.js");
+        BigNumber = require("bignumber.js"),
+        base64 = require("js-base64").Base64;
 
     module.exports = {
         components: {
@@ -420,6 +421,7 @@
                         api.getTransactionByContract({ address: o.address.hash }, this.$route.params.api, (data) => {
                             var data = JSON.parse(data);
                             this.contract = data.result ? data.result : {};
+                            this.obj.contractCode = base64.decode(this.contract.data);
                         })
                     }
                     this.txs = o.txList;
