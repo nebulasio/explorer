4a5525df0c6af28788fe9942bcda43418ba4275c
Young <cunxinaimei@126.com>
2018-12-04 14:36:10 +0800

    add link for github & twitter; fix the font color for tx list header;

--- explorer-front/src/components/vue-footer.vue
+++ explorer-front/src/components/vue-footer.vue
@@ -1,8 +1,8 @@
 <style>
     .vue-footer {
-        height:82px;
-        background:rgba(255,255,255,1);
-        box-shadow:0px 1px 0px 0px rgba(221,221,221,1);
+        height: 82px;
+        background: rgba(255, 255, 255, 1);
+        box-shadow: 0px 1px 0px 0px rgba(221, 221, 221, 1);
         margin-top: 30px;
         overflow: auto;
     }
@@ -25,20 +25,23 @@
         text-align: center;
         width: 14px;
     }
-    .div-left{
+
+    .div-left {
         float: left;
         width: 33.33%;
         height: 100%;
         vertical-align: middle;
     }
-    .div-middle{
+
+    .div-middle {
         float: left;
         width: 33.33%;
         height: 100%;
         text-align: center;
         vertical-align: middle;
     }
-    .div3{
+
+    .div3 {
         float: left;
         width: 33.34%;
         vertical-align: middle;
@@ -49,7 +52,8 @@
         <div class=container>
             <div class="div-left">
                 <span style="height: 100%;display: inline-block;vertical-align: middle;"></span>
-                <img style="height: 34px; width: 156px;display: inline; vertical-align: middle;" src="../../static/img/img_footer_logo.png" />
+                <img style="height: 34px; width: 156px;display: inline; vertical-align: middle;"
+                     src="../../static/img/img_footer_logo.png"/>
             </div>
             <div class="div-middle">
                 <span class="font-size-16-normal font-color-000000" style="line-height:82px;">
@@ -60,8 +64,14 @@
                 <span class="font-color-000000 font-size-16-normal" style="line-height: 82px;">
                     Submit a issue
                 </span>
-                <img class="icon24" style="vertical-align: middle;" src="../../static/img/ic_github.png"/>
-                <img class="icon24" style="vertical-align: middle;" src="../../static/img/ic_twitter.png"/>
+                <a target=_blank href=https://github.com/nebulasio/explorer/issues>
+                    <img class="icon24" style="vertical-align: middle;"
+                         src="../../static/img/ic_github.png"/>
+                </a>
+                <a target=_blank href=https://twitter.com/nebulasio>
+                    <img class="icon24" style="vertical-align: middle;"
+                         src="../../static/img/ic_twitter.png"/>
+                </a>
             </div>
         </div>
     </div>
