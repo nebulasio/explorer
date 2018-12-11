58dd516a9025586baa92b5830ee6fe5154fcc2e3
xihao.liao <xihao.liao@nebulas.io>
2018-12-11 11:51:06 +0800

    add redis config class，to keep mainnet and testnet use different redis database

---
+++ explorer-backend/src/main/java/io/nebulas/explorer/config/RedisConfig.java
@@ -0,0 +1,79 @@
+package io.nebulas.explorer.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
+    @Value("${redis.database}")
+    private int database;
+
+    @Bean(name = "customStringTemplate")
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate template = new StringRedisTemplate();
+        return (StringRedisTemplate) getRedisTemplate(template);
+    }
+
+    @Bean(name = "customRedisTemplate")
+    public RedisTemplate redisTemplate() {
+        RedisTemplate template = new RedisTemplate();
+        return getRedisTemplate(template);
+    }
+
+    public RedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory jedis = new JedisConnectionFactory();
+        jedis.setHostName(host);
+        jedis.setPort(port);
+
+        if (database != 0) {
+            jedis.setDatabase(database);
+        }
+        jedis.setPoolConfig(poolCofig());
+        // 初始化连接pool
+        jedis.afterPropertiesSet();
+        RedisConnectionFactory factory = jedis;
+        return factory;
+    }
+
+    public JedisPoolConfig poolCofig() {
+        JedisPoolConfig poolCofig = new JedisPoolConfig();
+        return poolCofig;
+    }
+
+    public RedisTemplate getRedisTemplate(RedisTemplate template) {
+        template.setConnectionFactory(connectionFactory());
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        //序列化 值时使用此序列化方法
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+}
