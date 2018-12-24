d45b79ec9a3ea4fce6040db7faf652148a0403c6
于宙Ryan <yuzhouwww@gmail.com>
2018-12-24 20:31:12 +0800

    build

--- explorer-front/src/assets/app-config.js
+++ explorer-front/src/assets/app-config.js
@@ -7,10 +7,10 @@ module.exports = {
         // first item is default
         mainnet: {
             name: "Mainnet",
-            // url: "http://52.53.225.118:32747/api/", // 旭存
+            url: "http://52.53.225.118:32747/api/", // 旭存
             // url: "http://172.16.15.124:8080/api/", // xihao
             // url: "http://39.96.38.130:8081/api/", //aliyun
-            url: "https://explorer.nebulas.io/main/api/", //正式
+            // url: "https://explorer.nebulas.io/main/api/", //正式
             atp: "n1zUNqeBPvsyrw5zxp9mKcDdLTjuaEL7s39"
         },
         testnet: {
