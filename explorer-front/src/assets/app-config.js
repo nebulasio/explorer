5dc0aacf40b68177d2ff54a88907b98dd7b4a567
于宙Ryan <yuzhouwww@gmail.com>
2019-01-04 21:34:56 +0800

    modify api url

--- explorer-front/src/assets/app-config.js
+++ explorer-front/src/assets/app-config.js
@@ -7,18 +7,18 @@ module.exports = {
         // first item is default
         mainnet: {
             name: "Mainnet",
-            url: "http://54.70.225.59:8081/api/", // 旭存
+            // url: "http://54.70.225.59:8081/api/", // 旭存
             // url: "http://172.16.15.124:8080/api/", // xihao
             // url: "http://39.96.38.130:8081/api/", //aliyun
-            // url: "https://explorer-test.nebulas.io/main/api/", //正式
+            url: "https://explorer-backend.nebulas.io/api/", //正式
             atp: "n1zUNqeBPvsyrw5zxp9mKcDdLTjuaEL7s39"
         },
         testnet: {
             name: "Testnet",
             // url: "http://172.16.15.124:8080/api/", // xihao
             // url: "http://18.188.124.226:8080/api/", // 公司测试服务器的ip
-            // url: "https://explorer.nebulas.io/test/api/", //正式
-            url: "http://39.96.38.130:8080/api/", //aliyun
+            url: "https://explorer-test-backend.nebulas/api/", //正式
+            // url: "http://39.96.38.130:8080/api/", //aliyun
             // url: "http://172.16.15.94:8080/api/", // xucun
             atp: "n1rR5uiy4vDUn7TPMAtJ8Y1Eo54K6EYvSJ6"
         }
