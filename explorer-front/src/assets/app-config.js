7481d613c23cdc649e3afba631fc1c310b8f9b1a
于宙Ryan <yuzhouwww@gmail.com>
2019-01-29 13:14:05 +0800

    update api config

--- explorer-front/src/assets/app-config.js
+++ explorer-front/src/assets/app-config.js
@@ -17,8 +17,8 @@ module.exports = {
             name: "Testnet",
             // url: "http://172.16.15.124:8080/api/", // xihao
             // url: "http://18.188.124.226:8080/api/", // 公司测试服务器的ip
-            url: "https://explorer-test-backend.nebulas.io/api/", //正式
-            // url: "http://39.96.38.130:8080/api/", //aliyun
+            // url: "https://explorer-test-backend.nebulas.io/api/", //正式
+            url: "http://39.96.38.130:8080/api/", //aliyun
             // url: "http://172.16.15.94:8080/api/", // xucun
             atp: "n1rR5uiy4vDUn7TPMAtJ8Y1Eo54K6EYvSJ6"
         }
