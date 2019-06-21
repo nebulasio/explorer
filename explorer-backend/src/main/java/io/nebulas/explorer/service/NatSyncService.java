102e167f862319fab0ebf9da44f951feef0278b2
xucun <xucun.yang@nebulas.io>
2019-06-21 16:00:34 +0800

    支持NAT合约间调用转账的处理

--- explorer-backend/src/main/java/io/nebulas/explorer/service/NatSyncService.java
+++ explorer-backend/src/main/java/io/nebulas/explorer/service/NatSyncService.java
@@ -7,6 +7,7 @@ import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -26,10 +27,13 @@ public class NatSyncService {
 
     private Executor singleExecutor = Executors.newSingleThreadExecutor();
 
+    private static final String TOPIC_INNER_CONTRACT = "chain.innerContract";
     private static final String TOPIC_NR = "chain.contract.nr";
     private static final String TOPIC_PLEDGE = "chain.contract.pledge";
     private static final String TOPIC_VOTE = "chain.contract.vote";
     private static final String TOPIC_NAT = "chain.contract.NATToken";
+
+    private static final String CONTRACT_NAT = "n1mpgNi6KKdSzr7i5Ma7JsG5yPY9knf9He7";
 
     @Autowired
     private NebApiServiceWrapper nebApiServiceWrapper;
@@ -50,6 +54,7 @@ public class NatSyncService {
                     GetEventsByHashResponse response = nebApiServiceWrapper.getEventsByHash(hash);
                     if (response != null) {
                         List<Event> events = response.getEvents();
+                        // 处理铸币生成的NAT
                         Event natEvent = null;
                         for (Event event : events) {
                             if (event.getTopic().equals(TOPIC_NR)) {
@@ -63,31 +68,31 @@ public class NatSyncService {
                                 natEvent = event;
                             }
                         }
-                        if (natEvent!=null){
+                        if (natEvent != null) {
                             // 真正的nat数据，需要解析
                             String data = natEvent.getData();
                             try {
                                 JSONObject jsonData = JSON.parseObject(data);
                                 if (jsonData.containsKey("Produce")) {
                                     JSONObject produce = jsonData.getJSONObject("Produce");
-                                    if (produce!=null && produce.containsKey("data")) {
+                                    if (produce != null && produce.containsKey("data")) {
                                         JSONArray natData = produce.getJSONArray("data");
                                         int size = natData.size();
-                                        for (int i=0; i<size; i++) {
+                                        for (int i = 0; i < size; i++) {
                                             NatRecord record = new NatRecord();
                                             JSONObject item = natData.getJSONObject(i);
                                             String address = item.getString("addr");
-                                            if (address==null) {
+                                            if (address == null) {
                                                 address = "";
                                             }
                                             String value = item.getString("value");
-                                            if (value==null || value.isEmpty()) {
+                                            if (value == null || value.isEmpty()) {
                                                 value = "0";
                                             }
                                             record.setSource(source);
                                             record.setAddress(address);
                                             record.setAmount(value);
-                                            record.setTimestamp(new Date(transaction.getTimestamp()*1000));
+                                            record.setTimestamp(new Date(transaction.getTimestamp() * 1000));
                                             record.setTxHash(transaction.getHash());
                                             record.setBlock(blockHeight);
                                             record.setCreatedAt(new Date());
@@ -97,6 +102,64 @@ public class NatSyncService {
                                 }
                             } catch (Exception e) {
                                 log.error("Nat sync error: {}", e.getMessage());
+                            }
+                        }
+                        // 处理合约间调用的转账NAT
+                        boolean hasInnerContractTransferNat = false;
+                        for (Event event : events) {
+                            try {
+                                if (event.getTopic().equals(TOPIC_INNER_CONTRACT)) {
+                                    JSONObject data = JSON.parseObject(event.getData());
+                                    if (data.containsKey("to")) {
+                                        String to = data.getString("to");
+                                        if (to.equals(CONTRACT_NAT)) {
+                                            hasInnerContractTransferNat = true;
+                                        }
+                                    }
+                                } else if (event.getTopic().equals(TOPIC_NAT)) {
+                                    if (!hasInnerContractTransferNat) {
+                                        continue;
+                                    }
+                                    JSONObject data = JSON.parseObject(event.getData());
+                                    if (!data.containsKey("Transfer")) {
+                                        continue;
+                                    }
+                                    boolean status = data.getBooleanValue("Status");
+                                    if (!status) {
+                                        hasInnerContractTransferNat = false;
+                                        continue;
+                                    }
+                                    //通过合约间调用进行的NAT转账
+                                    JSONObject transferData = data.getJSONObject("Transfer");
+                                    String from = transferData.getString("from");
+                                    String to = transferData.getString("to");
+                                    String value = transferData.getString("value");
+                                    BigDecimal valueDecimal = new BigDecimal(value);
+                                    BigDecimal valueDecimalConverse = valueDecimal.negate();
+
+                                    NatRecord recordForFromAddress = new NatRecord();
+                                    recordForFromAddress.setSource(NatRecord.SOURCE_UNKNOWN);
+                                    recordForFromAddress.setAddress(from);
+                                    recordForFromAddress.setAmount(valueDecimalConverse.toPlainString());
+                                    recordForFromAddress.setTimestamp(new Date(transaction.getTimestamp() * 1000));
+                                    recordForFromAddress.setTxHash(transaction.getHash());
+                                    recordForFromAddress.setBlock(blockHeight);
+                                    recordForFromAddress.setCreatedAt(new Date());
+                                    natRecordMapper.insert(recordForFromAddress);
+
+                                    NatRecord recordForToAddress = new NatRecord();
+                                    recordForToAddress.setSource(NatRecord.SOURCE_UNKNOWN);
+                                    recordForToAddress.setAddress(to);
+                                    recordForToAddress.setAmount(value);
+                                    recordForToAddress.setTimestamp(new Date(transaction.getTimestamp() * 1000));
+                                    recordForToAddress.setTxHash(transaction.getHash());
+                                    recordForToAddress.setBlock(blockHeight);
+                                    recordForToAddress.setCreatedAt(new Date());
+                                    natRecordMapper.insert(recordForToAddress);
+                                    hasInnerContractTransferNat = false;
+                                }
+                            } catch (Exception e) {
+                                log.error("Nat sync error - InnerContract: {}", e.getMessage());
                             }
                         }
                         break;
