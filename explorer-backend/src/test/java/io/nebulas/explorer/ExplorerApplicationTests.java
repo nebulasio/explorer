364275f6c8ddb5328e9a8035f40e1bee028ef454
xucun <xucun.yang@nebulas.io>
2019-09-04 21:41:30 +0800

    remove test code

--- explorer-backend/src/test/java/io/nebulas/explorer/ExplorerApplicationTests.java
+++ explorer-backend/src/test/java/io/nebulas/explorer/ExplorerApplicationTests.java
@@ -1,40 +1,40 @@
-package io.nebulas.explorer;
-
-import io.nebulas.explorer.domain.NaxPeriod;
-import io.nebulas.explorer.mapper.NaxPeriodMapper;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-
-import io.nebulas.explorer.service.blockchain.NebDipAwardService;
-import io.nebulas.explorer.service.thirdpart.nebulas.bean.Transaction;
-
-@ActiveProfiles("dev")
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ExplorerApplicationTests {
-
-    @Autowired
-    private NebDipAwardService dipAwardService;
-
-    @Autowired
-    private NaxPeriodMapper naxPeriodMapper;
-    /**
-     * 测试DIP交易的解析
-     * 运行多次可以检查是否存在重复插入
-     * @throws ParseException   with SimpleDateFormat
-     */
-    @Test
-    public void testDipTransactionParse() throws ParseException {
-
-    }
-
-}
+//package io.nebulas.explorer;
+//
+//import io.nebulas.explorer.domain.NaxPeriod;
+//import io.nebulas.explorer.mapper.NaxPeriodMapper;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.List;
+//
+//import io.nebulas.explorer.service.blockchain.NebDipAwardService;
+//import io.nebulas.explorer.service.thirdpart.nebulas.bean.Transaction;
+//
+//@ActiveProfiles("dev")
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ExplorerApplicationTests {
+//
+//    @Autowired
+//    private NebDipAwardService dipAwardService;
+//
+//    @Autowired
+//    private NaxPeriodMapper naxPeriodMapper;
+//    /**
+//     * 测试DIP交易的解析
+//     * 运行多次可以检查是否存在重复插入
+//     * @throws ParseException   with SimpleDateFormat
+//     */
+//    @Test
+//    public void testDipTransactionParse() throws ParseException {
+//
+//    }
+//
+//}
