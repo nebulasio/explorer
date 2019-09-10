14b77eebf98453bc7325f3ef983c69dbffae59c8
xucun <xucun.yang@nebulas.io>
2019-09-10 19:14:33 +0800

    revert v4 sql

--- explorer-backend/src/main/resources/db/migration/V4__nat_support.sql
+++ explorer-backend/src/main/resources/db/migration/V4__nat_support.sql
@@ -7,7 +7,7 @@ create table if not exists nat_record
   amount    varchar(64) default '',
   source    INTEGER,
   timestamp TIMESTAMP,
-  createdAt TIMESTAMP
+  createdAt TIMESTAMP default current_timestamp
 );
 
 create index nat_record_address_index
