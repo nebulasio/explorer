fc2a13d317b55e28360cc851184457539ca0a7f6
xucun <xucun.yang@nebulas.io>
2019-10-30 13:41:56 +0800

    optimize nrc20 tx list api(prod: use infra instead)

--- explorer-backend/src/main/java/io/nebulas/explorer/service/thirdpart/infra/bean/NRC20TxListBean.java
+++ explorer-backend/src/main/java/io/nebulas/explorer/service/thirdpart/infra/bean/NRC20TxListBean.java
@@ -4,4 +4,8 @@ import java.util.List;
 
 public class NRC20TxListBean {
     public List<NRC20Tx> txnList;
+    public int totalPage;
+    public int maxDisplayCnt;
+    public int currentPage;
+    public int txnCnt;
 }
