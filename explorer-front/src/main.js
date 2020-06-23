2bccc5dc3f2cd13b8673f6efa21e3e12a8edbaa2
includeleec <includeleec@gmail.com>
2020-06-23 14:14:02 +0800

    del useless code

--- explorer-front/src/main.js
+++ explorer-front/src/main.js
@@ -9,21 +9,11 @@ import App from "./App.vue";
 import api from "./api";
 import VueMobileDetection from "vue-mobile-detection";
 
-// import VueTranslate from "vue-translate-plugin";
-// import { EventBus } from "./events.js";
-// import { jsonStrings } from "./l10nstrings.js";
-
-// var appConfig = require("@/assets/app-config");
-
 Vue.prototype.$i18nRoute = Trans.i18nRoute.bind(Trans);
 Vue.config.productionTip = false;
 
-Vue.prototype.$api = api; // 将api挂载到vue的原型上
-
-// var vApp = {},
-// vAppConfig = require("@/assets/app-config"),
-// vRouter = new VueRouter({ routes: require("@/assets/routes") }),
-// gaPage = require("vue-analytics").page;
+// 将api挂载到vue的原型上
+Vue.prototype.$api = api;
 
 // Expose jQuery to the global object
 const jQuery = require("jquery");
@@ -44,8 +34,6 @@ Vue.use(IconsPlugin);
 // detect mobile view
 Vue.use(VueMobileDetection);
 
-// Vue.use(VueTranslate);
-
 // Install vue i18n
 Vue.use(VueI18n);
 
@@ -65,11 +53,6 @@ Vue.use(VueAnalytics, {
     sendHitTask: isProd
   }
 });
-
-// Vue.config.productionTip = false;
-// Vue.use(VueRouter);
-// vRouter.beforeEach(onBeforeEach);
-// vRouter.afterEach(onAfterEach);
 
 Number.prototype.pad = function(size) {
   var s = String(this);
@@ -118,27 +101,6 @@ Date.prototype.getWeekNumber = function() {
   return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
 };
 
-// vApp = new Vue({
-//   components: {
-//     //"vue-popmsg": require("@/components/vue-popmsg").default,
-//     "vue-footer": require("@/components/vue-footer").default,
-//     "vue-header": require("@/components/vue-header").default,
-//     "vue-modal": require("@/components/vue-modal").default
-//   },
-//   data: {
-//     timestamp: Date.now(),
-//     showModalLoading: false,
-//     showAtpAds: true,
-//     mainnetDipStarted: true,
-//     mainnetGotDipWinners: true,
-//     testnetDipStarted: true,
-//     testnetGotDipWinners: true
-//   },
-//   el: ".vue",
-//   i18n,
-//   router
-// });
-
 new Vue({
   data: {
     showModalLoading: false
@@ -147,107 +109,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
-
-// Vue.prototype.$selectedLanguage = "en_US";
-
-// setInterval(() => {
-//   vApp.timestamp = Date.now();
-// }, 1000);
-
-////////////////////////////////////////////////////////////
-//
-// API PREFIX
-//
-////////////////////////////////////////////////////////////
-
-// function onBeforeEach(to, from, next) {
-//   window.scrollTo(0, 0);
-
-//   vApp.showModalLoading = false;
-
-//   var apiPrefix, first, path;
-
-//   for (first in vAppConfig.apiPrefixes) break;
-
-//   if (to.name == "*") {
-//     path = (from.params.api ? "/" + from.params.api : "") + "/404";
-//   } else if (to.params.api) {
-//     if (to.params.api in vAppConfig.apiPrefixes) {
-//       if (to.params.api == first) {
-//         // mainnet/xxx -> /xxx
-//         to.params.api = undefined;
-//         path = vRouter.resolve({ params: to.params }, to).resolved.fullPath;
-//       } else {
-//         apiPrefix = vAppConfig.apiPrefixes[to.params.api].url;
-//       }
-//     } else {
-//       path = (from.params.api ? "/" + from.params.api : "") + "/404";
-//     }
-//   } else {
-//     apiPrefix = vAppConfig.apiPrefixes[first].url;
-//   }
-
-//   sessionStorage.apiPrefix = apiPrefix;
-//   next(path);
-// }
-
-// function onAfterEach(to, from) {
-//   if (to.meta && to.meta.uaview) {
-//     gaPage(to.meta.uaview);
-//   }
-// }
-
-// Localization bar
-// var myComp = Vue.extend({
-//   template: `<div>
-// 						<a class="nav-link" href=# id=language-selector role=button data-toggle=dropdown aria-haspopup=true aria-expanded=false>
-// 							{{ t('Languages') }}
-// 							<img src=/static/img/icon_arrow_down.png width=12 alt="">
-// 						</a>
-// 						<div class=dropdown-menu aria-labelledby=language-selector>
-// 							<div>
-// 								<a href="#" class="dropdown-item" v-on:click.prevent=translateToEnglish()><img src=/static/img/icon-engflag.png width=12 alt=""> {{ t('English') }}</a>
-// 								<a href="#" class="dropdown-item" v-on:click.prevent=translateToSpanish()><img src=/static/img/icon-espflag.png width=12 alt=""> {{ t('Spanish') }}</a>
-// 							</div>
-// 						</div>
-// 					</div>`,
-//   locales: {
-//     es_ES: {
-//       Languages: "Idiomas",
-//       English: "Inglés",
-//       Spanish: "Español"
-//     },
-//     en_US: {
-//       Languages: "Languages",
-//       English: "English",
-//       Spanish: "Spanish"
-//     }
-//   },
-//   methods: {
-//     translateToSpanish() {
-//       Vue.prototype.$selectedLanguage = "es_ES";
-//       this.$translate.setLang(Vue.prototype.$selectedLanguage);
-//       EventBus.$emit("changeLanguage");
-//     },
-//     translateToEnglish() {
-//       Vue.prototype.$selectedLanguage = "en_US";
-//       this.$translate.setLang(Vue.prototype.$selectedLanguage);
-//       EventBus.$emit("changeLanguage");
-//     },
-//     initializeApp() {
-//       EventBus.$emit("changeLanguage");
-//       clearInterval(this.longIntervalID);
-//     }
-//   },
-//   mounted() {
-//     this.longIntervalID = setInterval(() => {
-//       this.initializeApp();
-//     }, 1500);
-//   }
-// });
-
-// var header0 = new Vue({
-//   el: "#lang-sel",
-//   components: { myComp },
-//   template: `<div><my-comp></my-comp></div>`
-// });
